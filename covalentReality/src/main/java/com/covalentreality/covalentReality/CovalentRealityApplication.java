885eea60e2055311a58547682035a65fea689750
alihamie <ali.hamie.29@gmail.com>
2018-04-29 22:17:21 -0700

    Adding initial repo

---
+++ covalentReality/src/main/java/com/covalentreality/covalentReality/CovalentRealityApplication.java
@@ -0,0 +1,13 @@
+package com.covalentreality.covalentReality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovalentRealityApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CovalentRealityApplication.class, args);
+	}
+}
